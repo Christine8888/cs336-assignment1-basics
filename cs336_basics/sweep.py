d1e58579f8ae954bb766dfe20c5101e6e3775edf
Christine Ye <christineye88@outlook.com>
2025-04-13 17:26:47 -0700

    sweep code

---
+++ cs336_basics/sweep.py
@@ -0,0 +1,25 @@
+import hydra
+import subprocess
+from omegaconf import DictConfig, OmegaConf
+import os
+
+@hydra.main(config_path="conf", config_name="config")
+def run_sweep(cfg: DictConfig):
+    # Create command line arguments from config
+    cmd = ["python", "train.py"]
+    
+    for k, v in cfg.train_args.items():
+        cmd.extend([f"--{k}", str(v)])
+    
+    # print and run command correspodning to sweep
+    print(f"Running command: {' '.join(cmd)}")
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    
+    # print outfits
+    print(f"STDOUT:\n{result.stdout}")
+    print(f"STDERR:\n{result.stderr}")
+    
+    return result.returncode
+
+if __name__ == "__main__":
+    run_sweep()
